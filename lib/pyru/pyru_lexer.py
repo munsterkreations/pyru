eaa2d2b3b4d1ec5adc56e9160e41f2ae7b7796d5
munsterkreations <104373517+munsterkreations@users.noreply.github.com>
2026-01-30 03:59:30 -0600

    Update pyru_lexer.py

--- lib/pyru/pyru_lexer.py
+++ lib/pyru/pyru_lexer.py
@@ -1,102 +1,36 @@
 import re
+import yaml
+from pathlib import Path
 
-class PyruLexer:
-    def __init__(self, code):
-        self.code = code
-        self.tokens = []
-        self.current_position = 0
+TOKEN_SPEC_PATH = Path(__file__).parents[2] / "spec" / "tokens.yml"
 
-    def tokenize(self):
-        while self.current_position < len(self.code):
-            char = self.code[self.current_position]
-            if char.isspace():
-                self.current_position += 1
-            elif char.isalpha() or char == '_':
-                self.tokenize_identifier()
-            elif char.isdigit():
-                self.tokenize_number()
-            elif char == '"':
-                self.tokenize_string()
-            elif char in ('+', '-', '*', '/', '%', '=', '<', '>', '!', '&', '|'):
-                self.tokenize_operator()
-            elif char in ('(', ')', '[', ']', '{', '}', ',', ':'):
-                self.tokens.append((char, "PUNCTUATION"))
-                self.current_position += 1
-            elif char == '#':
-                self.tokenize_comment()
-            else:
-                raise Exception(f"Invalid character '{char}' at position {self.current_position}")
+class Lexer:
+    def __init__(self):
+        spec = yaml.safe_load(TOKEN_SPEC_PATH.read_text())
+        self.rules = []
 
-        return self.tokens
+        for name, rule in spec.items():
+            if "regex" in rule:
+                self.rules.append((name, re.compile(rule["regex"])))
+            elif isinstance(rule, str):
+                self.rules.append((name, re.compile(re.escape(rule))))
 
-    def tokenize_identifier(self):
-        identifier = ''
-        while self.current_position < len(self.code):
-            char = self.code[self.current_position]
-            if char.isalnum() or char == '_':
-                identifier += char
-                self.current_position += 1
-            else:
-                self.tokens.append((identifier, "IDENTIFIER"))
-                return
+    def tokenize(self, text):
+        pos = 0
+        tokens = []
 
-    def tokenize_number(self):
-        number = ''
-        while self.current_position < len(self.code):
-            char = self.code[self.current_position]
-            if char.isdigit() or char == '.':
-                number += char
-                self.current_position += 1
-            else:
-                self.tokens.append((number, "NUMBER"))
-                return
+        while pos < len(text):
+            match = None
+            for name, regex in self.rules:
+                match = regex.match(text, pos)
+                if match:
+                    tokens.append((name, match.group()))
+                    pos = match.end()
+                    break
 
-    def tokenize_string(self):
-        string = ''
-        self.current_position += 1
-        while self.current_position < len(self.code):
-            char = self.code[self.current_position]
-            if char != '"':
-                string += char
-                self.current_position += 1
-            else:
-                self.tokens.append((string, "STRING"))
-                self.current_position += 1
-                return
-        raise Exception("Unterminated string")
+            if not match:
+                raise SyntaxError(f"Illegal character at {pos}")
 
-    def tokenize_operator(self):
-        operator = ''
-        while self.current_position < len(self.code):
-            char = self.code[self.current_position]
-            if char in ('+', '-', '*', '/', '%', '=', '<', '>', '!', '&', '|'):
-                operator += char
-                self.current_position += 1
-            else:
-                self.tokens.append((operator, "OPERATOR"))
-                return
-
-    def tokenize_comment(self):
-        comment = ''
-        while self.current_position < len(self.code):
-            char = self.code[self.current_position]
-            if char != '\n':
-                comment += char
-                self.current_position += 1
-            else:
-                self.tokens.append((comment, "COMMENT"))
-                self.current_position += 1
-                return
-
-# Example usage:
-code = """
-# This is a comment
-x = 10
-y = 20
-print("Sum:", x + y)
-"""
-
-lexer = PyruLexer(code)
-tokens = lexer.tokenize()
-for token in tokens:
-    print(token)
+        tokens.append(("EOF", None))
+        return tokens
+        
